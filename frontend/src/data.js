75a21a1993c0c8aafaf3cbd71bc19aa473872f9c
DumbCodes <sandeshpoudel@students.federation.edu.au>
2022-08-04 22:54:34 +0930

    Adding a list

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Pink',
+      aka: 'nike-women-pink',
+      category: 'Women',
+      image: '/images/p1.jpg',
+      price: 170,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'High quality running and everyday shoe. Designed by Nike sold by Sneakr Gemz',
+    },
+    {
+      name: 'Nike White',
+      aka: 'Nike-man-white',
+      category: 'Men',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        'High quality Party and Everyday use. Designed by Nike sold by Sneakr Gemz',
+    },
+    {
+      name: 'Nike Blue',
+      aka: 'nike-kids-blue',
+      category: 'Kids',
+      image: '/images/p3.jpg',
+      price: 70,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description:
+        'High quality everyday shoe for kids. Designed by Nike sold by Sneakr Gemz',
+    },
+    {
+      name: 'Nike Colorful',
+      aka: 'Nike-unisex-colorful',
+      category: 'women',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Great design everyday shoe. Designed by Nike sold by Sneakr Gemz',
+    },
+    {
+      name: 'Nike Curve',
+      aka: 'Nike-women-curve',
+      category: 'women',
+      image: '/images/p5.jpg',
+      price: 190,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Great design everyday shoe superb flexible. Designed by Nike sold by Sneakr Gemz',
+    },
+    {
+      name: 'Nike Blacktik',
+      aka: 'Nike-men-blacktik',
+      category: 'men',
+      image: '/images/p6.jpg',
+      price: 160,
+      countInStock: 0,
+      brand: 'Nike',
+      rating: 4.8,
+      numReviews: 10,
+      description:
+        'Great design everyday shoe best for casual wear. Designed by Nike sold by Sneakr Gemz',
+    },
+  ],
+};
+export default data;
